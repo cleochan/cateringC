bc800124b9fdd291269ca2a804d65bd7ca805c35
cleochan <mark560@gmail.com>
2014-05-23 11:35:36 +0800

    a

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,11 +4,39 @@ class IndexController extends Zend_Controller_Action
 {
     function indexAction()
     {
-    	$mod_test = new Algorithms_Extensions_Test();
-    	$mod_test->echostr = "HeyHey";
-    	$mod_test->valid();
-    	
-    	echo "<br /><br />End.</br />";
+    	//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	
+		//extract post data
+		if (!empty($postStr)){
+	
+			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+			$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$keyword = trim($postObj->Content);
+			$time = time();
+			$textTpl = "<xml>
+						<ToUserName><![CDATA[toUser]]></ToUserName>
+						<FromUserName><![CDATA[fromUser]]></FromUserName>
+						<CreateTime>12345678</CreateTime>
+						<MsgType><![CDATA[text]]></MsgType>
+						<Content><![CDATA[你好]]></Content>
+						</xml>";
+			if(!empty( $keyword ))
+			{
+				$msgType = "text";
+				$contentStr = "Welcome to wechat world!";
+				$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+				echo $resultStr;
+			}else{
+				echo "Input something...";
+			}
+	
+		}else {
+			echo "";
+			exit;
+		}
+		
 		die;
     }
 }
