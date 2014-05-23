274bc62ea8bf3ad111aaa0a9173a27850a889210
cleochan <mark560@gmail.com>
2014-05-23 11:12:20 +0800

    hey

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,6 +5,7 @@ class IndexController extends Zend_Controller_Action
     function indexAction()
     {
     	$mod_test = new Algorithms_Extensions_Test();
+    	$mod_test->echostr = "HeyHey";
     	$mod_test->valid();
     	
     	echo "<br /><br />End.</br />";
