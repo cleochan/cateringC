4b6b568a9a2800cded2fff31e33847285f686e43
cleochan <mark560@gmail.com>
2014-05-27 18:31:03 +0800

    a

--- application/models/Databases/Tables/Orders.php
+++ application/models/Databases/Tables/Orders.php
@@ -28,33 +28,30 @@ class Databases_Tables_Orders extends Zend_Db_Table
     	
     	if($this->orders_channel && $this->orders_type && $this->orders_items)
     	{
-    		if($order_code)
+    		$row = $this->createRow();
+    		$row->orders_channel = $this->orders_channel;
+    		$row->orders_time = $this->orders_time;
+    		$row->orders_status = $this->orders_status;
+    		$row->orders_payment_status = $this->orders_payment_status;
+    		$row->orders_type = $this->orders_type;
+    		$row->orders_amount = $this->orders_amount;
+    		$row->orders_cash = $this->orders_cash;
+    		$row->orders_change = $this->orders_change;
+    		$row->orders_subtotal = $this->orders_subtotal;
+    		$row->orders_coupon = $this->orders_coupon;
+    		$row->orders_discount = $this->orders_discount;
+    		$row->orders_member_id = $this->orders_member_id;
+    		$row->table_id = $this->table_id;
+    		$row->users_id = $_SESSION['user_info']['users_id'];
+    		$orders_id = $row->save();
+    		
+    		//insert items
+    		if($orders_id)
     		{
-    			$row = $this->createRow();
-    			$row->orders_channel = $this->orders_channel;
-    			$row->orders_time = $this->orders_time;
-    			$row->orders_status = $this->orders_status;
-    			$row->orders_payment_status = $this->orders_payment_status;
-    			$row->orders_type = $this->orders_type;
-    			$row->orders_amount = $this->orders_amount;
-    			$row->orders_cash = $this->orders_cash;
-    			$row->orders_change = $this->orders_change;
-    			$row->orders_subtotal = $this->orders_subtotal;
-    			$row->orders_coupon = $this->orders_coupon;
-    			$row->orders_discount = $this->orders_discount;
-    			$row->orders_member_id = $this->orders_member_id;
-    			$row->table_id = $this->table_id;
-    			$row->users_id = $_SESSION['user_info']['users_id'];
-    			$orders_id = $row->save();
-    			
-    			//insert items
-    			if($orders_id)
-    			{
-    				$mod_orders_contains = new Databases_Tables_OrdersContains();
-    				$mod_orders_contains->orders_id = $orders_id;
-    				$mod_orders_contains->items_array = $this->orders_items;
-    				$result = $mod_orders_contains->InsertItems();
-    			}
+    			$mod_orders_contains = new Databases_Tables_OrdersContains();
+    			$mod_orders_contains->orders_id = $orders_id;
+    			$mod_orders_contains->items_array = $this->orders_items;
+    			$result = $mod_orders_contains->InsertItems();
     		}
     	}
     	
