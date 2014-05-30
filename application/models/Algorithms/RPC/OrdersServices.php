a4adf20a6dd4c0d703a9589050b2dca3e29efec7
cleochan <mark560@gmail.com>
2014-05-30 09:15:24 +0800

    a

--- application/models/Algorithms/RPC/OrdersServices.php
+++ application/models/Algorithms/RPC/OrdersServices.php
@@ -47,4 +47,73 @@ class Algorithms_RPC_OrdersServices
     	
     	return $result;
     }
+    
+    function UpdateLogSync($post_array)
+    {
+    	$result = 0;
+    	
+    	if(!empty($post_array))
+    	{
+    		$mod_log_sync = new Databases_Tables_LogSync();
+    		$event_array = array();
+    		
+    		//data restructure
+    		foreach($post_array as $pa)
+    		{
+    			// add sync log
+    			$mod_log_sync->log_time = $pa['log_time'];
+    			$mod_log_sync->log_event = $pa['log_event'];
+    			$mod_log_sync->log_key = $pa['log_key'];
+    			if($pa['log_val'])
+    			{
+    				$mod_log_sync->log_val = $pa['log_val'];
+    			}
+    			$mod_log_sync->AddLog();
+    			
+    			//combine array
+    			if(!$event_array[$pa['log_event']])
+    			{
+    				$event_array[$pa['log_event']] = array();
+    			}
+    			
+    			$event_array[$pa['log_event']][$pa['log_key']] = $pa['log_val'];
+    		}
+    		
+    		//proceed
+    		
+    		/**
+    		 * UPDATE_ORDER_STATUS
+    		 */
+    		if(!empty($event_array['UPDATE_ORDER_STATUS']))
+    		{
+    			$mod_orders = new Databases_Tables_Orders();
+    			
+    			foreach ($event_array['UPDATE_ORDER_STATUS'] as $key => $val)
+    			{
+    				//initial update, need to update order code and payment status
+    				if($val['order_code'])
+    				{
+    					$row = $mod_orders->fetchRow("orders_id = '".$key."'");
+    					if(!empty($row))
+    					{
+    						$row->orders_code = $val['order_code'];
+    						$row->orders_status = $val['status'];
+    						$row->orders_payment_status = 1; //Paid
+    						$row->save();
+    						
+    						$result += 1;
+    					}
+    				}else{ //update status only
+    					$mod_orders->orders_id = $key;
+    					$mod_orders->orders_status = $val['status'];
+    					$mod_orders->UpdateStatus();
+    					
+    					$result += 1;
+    				}
+    			}
+    		}
+    	}
+    	
+    	return $result;
+    }
 }
---
+++ application/models/Databases/Tables/LogSync.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * LogSync
+ * @author peng.chen
+ *
+ * ===== Event Instruction =====
+ * 
+ * UPDATE_ORDER_STATUS
+ * UPDATE_PRODUCT_INFO
+ * UPDATE_SETS_INFO
+ * UPDATE_PRODUCT_STATUS
+ * ADD_NEW_PRODUCT
+ * ADD_NEW_SETS
+ * 
+ * =============================
+ */
+
+class Databases_Tables_LogSync extends Zend_Db_Table
+{
+    protected $_name = 'log-sync';
+    var $log_event;
+    var $log_key;
+    var $log_val;
+    var $log_time;
+    
+    function AddLog()
+    {
+    	$result = NULL;
+    	
+    	if($this->log_event && $this->log_key)
+    	{
+    		$row = $this->createRow();
+    		$row->log_time = $this->log_time;
+    		$row->log_event = $this->log_event;
+    		$row->log_key = $this->log_key;
+    		if($this->log_val)
+    		{
+    			$row->log_val = $this->log_val;
+    		}
+    		$row->log_updated_time = date("Y-m-d H:i:s");
+    		
+    		$result = $row->save();
+    	}
+    	
+    	return $result;
+    }
+}
