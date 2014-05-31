5caa70a7bb8dba3f2f7521a4262c6fc9de8a08e6
cleochan <mark560@gmail.com>
2014-05-31 21:06:41 +0800

    a

--- application/controllers/PluginController.php
+++ application/controllers/PluginController.php
@@ -13,4 +13,16 @@ class PluginController extends Zend_Controller_Action
 		$mod->FetchNewOrdersServer();
 		die;
 	}
+	
+	function test1Action()
+	{
+		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=http%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect");
+		die;
+	}
+	
+	function test2Action()
+	{
+		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
+		die;
+	}
 }
