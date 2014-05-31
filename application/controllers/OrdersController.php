feed2eb27b39e2f152c7f0d234cb39796381776e
cleochan <mark560@gmail.com>
2014-05-31 22:19:53 +0800

    a

--- application/controllers/OrdersController.php
+++ application/controllers/OrdersController.php
@@ -9,34 +9,37 @@ class OrdersController extends Zend_Controller_Action
 	
 	function preDispatch()
 	{
-		//check identity
-		$params = $this->_request->getParams();
-		$mod_admin = new Databases_Tables_Admin();
-		
-		if($params['session_id'])
-    	{
-    		$_SESSION['admin_session'] = $params['session_id'];
-    	}
-    	
-    	if($_SESSION['admin_session']){
-    		$mod_params = new Databases_Tables_Params();
-    		if($_SESSION['admin_session'] === $mod_params->GetVal('TestSession'))
-    		{
-    			$fake_admin = $mod_admin->fetchRow("admin_id=2");
-    			$admin_info = $fake_admin->toArray();
-    		}else{
-    			$admin_info = $mod_admin->CheckSessionValidation();
-    		}
-    		
-    		if(empty($admin_info))
-    		{
-    			$error = 1005;
-    		}else{
-    			$_SESSION['admin_info'] = $admin_info;
-    		}
-    	}else{
-    		$error = 1002;
-    	}
+		if(!$_SESSION['admin_info'])
+		{
+			//check identity
+			$params = $this->_request->getParams();
+			$mod_admin = new Databases_Tables_Admin();
+			
+			if($params['session_id'])
+			{
+				$_SESSION['admin_session'] = $params['session_id'];
+			}
+			 
+			if($_SESSION['admin_session']){
+				$mod_params = new Databases_Tables_Params();
+				if($_SESSION['admin_session'] === $mod_params->GetVal('TestSession'))
+				{
+					$fake_admin = $mod_admin->fetchRow("admin_id=2");
+					$admin_info = $fake_admin->toArray();
+				}else{
+					$admin_info = $mod_admin->CheckSessionValidation();
+				}
+			
+				if(empty($admin_info))
+				{
+					$error = 1005;
+				}else{
+					$_SESSION['admin_info'] = $admin_info;
+				}
+			}else{
+				$error = 1002;
+			}
+		}
     	
     	//register error
     	if($error)
--- application/controllers/PluginController.php
+++ application/controllers/PluginController.php
@@ -22,12 +22,6 @@ class PluginController extends Zend_Controller_Action
 		die;
 	}
 	
-	function test2Action()
-	{
-		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=something123#wechat_redirect");
-		die;
-	}
-	
 	function test3Action()
 	{
 		$params = $this->_request->getParams();
