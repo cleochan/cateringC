c9f679597b37deabb85c314ffb475131ed975054
cleochan <mark560@gmail.com>
2014-05-31 21:16:03 +0800

    d

--- application/controllers/PluginController.php
+++ application/controllers/PluginController.php
@@ -16,13 +16,23 @@ class PluginController extends Zend_Controller_Action
 	
 	function test1Action()
 	{
-		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=http%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect");
+		$redirect_uri = urlencode("http://wechat.jushulin.mobi/plugin/test-target");
+		
+		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8e7db22a86a79ad8&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_base&state=123#wechat_redirect");
 		die;
 	}
 	
 	function test2Action()
 	{
-		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
+		$this->_redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=something123#wechat_redirect");
+		die;
+	}
+	
+	function testTargetAction()
+	{
+		$params = $this->_request->getParams();
+		
+		var_dump($params);
 		die;
 	}
 }
