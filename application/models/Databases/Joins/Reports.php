1724850f47c3507d9f39d3648ae8b4dd8b67e7b3
cleochan <mark560@gmail.com>
2014-11-09 18:28:55 +0800

    new report

--- application/models/Databases/Joins/Reports.php
+++ application/models/Databases/Joins/Reports.php
@@ -47,11 +47,15 @@ class Databases_Joins_Reports
     
     function MakeReportRevList($range)
     {
+    	$mod_plugin = new Algorithms_Extensions_Plugin();
+    	
     	$result_key = array();
     	$result_val = array();
+    	$result_customer_qty = array();
+    	$result_avg_sale = array();
     	$result_hour_key = array();
     	$result_hour_val = array();
-    	 
+    	
     	if('day' == $range)
     	{
     		$data = $this->db->select();
@@ -60,7 +64,7 @@ class Databases_Joins_Reports
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -69,20 +73,43 @@ class Databases_Joins_Reports
     				$result_val[] = $row['amount'];
     			}
     		}
-    
+    		
+    		//计算人数和人均消费
+    		$data = $this->db->select();
+    		$data->from("report-rev", array("date(order_time) as dt", "sum(order_amount) as amount", "sum(customer_qty) as qty"));
+    		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-1,date("d"),date("Y"))));
+    		$data->where("order_channel = ?", 1); //仅限堂食
+    		$data->group("dt");
+    		$data->order("dt ASC");
+    		$rows = $this->db->fetchAll($data);
+    		
+    		if(!empty($rows))
+    		{
+    			foreach($rows as $row)
+    			{
+    				if($row['qty'])
+    				{
+    					$result_customer_qty[] = $row['qty'];
+    					$result_avg_sale[] = $mod_plugin->FormatPrice($row['amount'] / $row['qty']);
+    				}else{
+    					$result_customer_qty[] = 0;
+    					$result_avg_sale[] = 0;
+    				}
+    			}
+    		}
+    		
     		//按时段
     		$data = $this->db->select();
     		$data->from("report-rev", array("order_time", "order_amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-1,date("d"),date("Y"))));
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		/*
-    		 * lunch 08:00 - 13:59
-    		* afternoon 14:00 - 16:59
-    		* dinner 17:00 - 19:59
-    		* night 20:00 - 21:59
-    		* midnight 22:00 - 01:59
-    		*/
+    		 * lunch 09:00 - 14:00
+    		 * afternoon 14:00 - 17:00
+    		 * dinner 17:00 - 21:00
+    		 * night 21:00 - 01:00
+    		 */
     		$result_lunch_key = array();
     		$result_lunch_val = array();
     		$result_afternoon_key = array();
@@ -91,70 +118,59 @@ class Databases_Joins_Reports
     		$result_dinner_val = array();
     		$result_night_key = array();
     		$result_night_val = array();
-    		$result_midnight_key = array();
-    		$result_midnight_val = array();
-    
+    		
     		$lunch = array();
     		$afternoon = array();
     		$dinner = array();
     		$night = array();
-    		$midnight = array();
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
     			{
     				$hour = substr($row['order_time'],11,2);
     				$min = substr($row['order_time'],14,2);
-    
+    				
     				$day = substr($row['order_time'],5,5);
-    
-    				if(in_array($hour, array(8,9,10,11,12,13,'08','09')))
+    				
+    				if(in_array($hour, array(9,10,11,12,13,'09')))
     				{
     					if(!$lunch[$day])
     					{
     						$lunch[$day] = 0;
     					}
-    						
+    					
     					$lunch[$day] += $row['order_amount'];
-    						
+    					
     				}elseif(in_array($hour, array(14,15,16)))
     				{
     					if(!$afternoon[$day])
     					{
     						$afternoon[$day] = 0;
     					}
-    						
+    					
     					$afternoon[$day] += $row['order_amount'];
-    						
-    				}elseif(in_array($hour, array(17,18,19)))
+    					
+    				}elseif(in_array($hour, array(17,18,19,20)))
     				{
     					if(!$dinner[$day])
     					{
     						$dinner[$day] = 0;
     					}
-    						
+    					
     					$dinner[$day] += $row['order_amount'];
-    						
-    				}elseif(in_array($hour, array(20,21)))
+    					
+    				}elseif(in_array($hour, array(21,22,23,0,'00')))
     				{
     					if(!$night[$day])
     					{
     						$night[$day] = 0;
     					}
-    						
+    					
     					$night[$day] += $row['order_amount'];
-    				}elseif(in_array($hour, array(0,1,22,23,'00','01')))
-    				{
-    					if(!$midnight[$day])
-    					{
-    						$midnight[$day] = 0;
-    					}
-    						
-    					$midnight[$day] += $row['order_amount'];
     				}
     			}
-    			 
+    			
     			if(!empty($lunch))
     			{
     				foreach($lunch as $lunch_key => $lunch_val)
@@ -163,7 +179,7 @@ class Databases_Joins_Reports
     					$result_lunch_val[] = $lunch_val;
     				}
     			}
-    			 
+    			
     			if(!empty($afternoon))
     			{
     				foreach($afternoon as $afternoon_key => $afternoon_val)
@@ -172,7 +188,7 @@ class Databases_Joins_Reports
     					$result_afternoon_val[] = $afternoon_val;
     				}
     			}
-    			 
+    			
     			if(!empty($dinner))
     			{
     				foreach($dinner as $dinner_key => $dinner_val)
@@ -181,7 +197,7 @@ class Databases_Joins_Reports
     					$result_dinner_val[] = $dinner_val;
     				}
     			}
-    			 
+    			
     			if(!empty($night))
     			{
     				foreach($night as $night_key => $night_val)
@@ -190,24 +206,15 @@ class Databases_Joins_Reports
     					$result_night_val[] = $night_val;
     				}
     			}
-    			 
-    			if(!empty($midnight))
-    			{
-    				foreach($midnight as $midnight_key => $midnight_val)
-    				{
-    					$result_midnight_key[] = $midnight_key;
-    					$result_midnight_val[] = $midnight_val;
-    				}
-    			}
     		}
-    
+    		
     		$data = $this->db->select();
     		$data->from("report-rev", array("hour(order_time) as dt", "sum(order_amount) as amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-1,date("d"),date("Y"))));
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -224,7 +231,7 @@ class Databases_Joins_Reports
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -233,20 +240,43 @@ class Databases_Joins_Reports
     				$result_val[] = $row['amount'];
     			}
     		}
-    
+    		
+    		//计算人数和人均消费
+    		$data = $this->db->select();
+    		$data->from("report-rev", array("week(order_time,1) as dt", "sum(order_amount) as amount", "sum(customer_qty) as qty"));
+    		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
+    		$data->where("order_channel = ?", 1); //仅限堂食
+    		$data->group("dt");
+    		$data->order("dt ASC");
+    		$rows = $this->db->fetchAll($data);
+    		
+    		if(!empty($rows))
+    		{
+    			foreach($rows as $row)
+    			{
+    				if($row['qty'])
+    				{
+    					$result_customer_qty[] = $row['qty'];
+    					$result_avg_sale[] = $mod_plugin->FormatPrice($row['amount'] / $row['qty']);
+    				}else{
+    					$result_customer_qty[] = 0;
+    					$result_avg_sale[] = 0;
+    				}
+    			}
+    		}
+    		
     		//按时段
     		$data = $this->db->select();
     		$data->from("report-rev", array("order_time", "order_amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		/*
-    		 * lunch 08:00 - 13:59
-    		* afternoon 14:00 - 16:59
-    		* dinner 17:00 - 19:59
-    		* night 20:00 - 21:59
-    		* midnight 22:00 - 01:59
-    		*/
+    		 * lunch 09:00 - 14:00
+    		 * afternoon 14:00 - 17:00
+    		 * dinner 17:00 - 21:00
+    		 * night 21:00 - 01:00
+    		 */
     		$result_lunch_key = array();
     		$result_lunch_val = array();
     		$result_afternoon_key = array();
@@ -255,72 +285,60 @@ class Databases_Joins_Reports
     		$result_dinner_val = array();
     		$result_night_key = array();
     		$result_night_val = array();
-    		$result_midnight_key = array();
-    		$result_midnight_val = array();
-    
+    		
     		$lunch = array();
     		$afternoon = array();
     		$dinner = array();
     		$night = array();
-    		$midnight = array();
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
     			{
     				$hour = substr($row['order_time'],11,2);
     				$min = substr($row['order_time'],14,2);
-    
+    		
     				$week = date("W", mktime(0,0,0,substr($row['order_time'],5,2),substr($row['order_time'],8,2),substr($row['order_time'],0,4)));
-    
-    				if(in_array($hour, array(8,9,10,11,12,13,'08','09')))
+    		
+    				if(in_array($hour, array(9,10,11,12,13,'09')))
     				{
     					if(!$lunch[$week])
     					{
     						$lunch[$week] = 0;
     					}
-    
+    						
     					$lunch[$week] += $row['order_amount'];
-    
+    						
     				}elseif(in_array($hour, array(14,15,16)))
     				{
     					if(!$afternoon[$week])
     					{
     						$afternoon[$week] = 0;
     					}
-    
+    						
     					$afternoon[$week] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(17,18,19)))
+    						
+    				}elseif(in_array($hour, array(17,18,19,20)))
     				{
     					if(!$dinner[$week])
     					{
     						$dinner[$week] = 0;
     					}
-    
+    						
     					$dinner[$week] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(20,21)))
+    						
+    				}elseif(in_array($hour, array(21,22,23,0,'00')))
     				{
     					if(!$night[$week])
     					{
     						$night[$week] = 0;
     					}
-    
+    						
     					$night[$week] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(22,23,0,1,'00','01')))
-    				{
-    					if(!$midnight[$week])
-    					{
-    						$midnight[$week] = 0;
-    					}
-    
-    					$midnight[$week] += $row['order_amount'];
-    
+    						
     				}
     			}
-    
+    			 
     			if(!empty($lunch))
     			{
     				foreach($lunch as $lunch_key => $lunch_val)
@@ -329,7 +347,7 @@ class Databases_Joins_Reports
     					$result_lunch_val[] = $lunch_val;
     				}
     			}
-    
+    			 
     			if(!empty($afternoon))
     			{
     				foreach($afternoon as $afternoon_key => $afternoon_val)
@@ -338,7 +356,7 @@ class Databases_Joins_Reports
     					$result_afternoon_val[] = $afternoon_val;
     				}
     			}
-    
+    			 
     			if(!empty($dinner))
     			{
     				foreach($dinner as $dinner_key => $dinner_val)
@@ -347,7 +365,7 @@ class Databases_Joins_Reports
     					$result_dinner_val[] = $dinner_val;
     				}
     			}
-    
+    			 
     			if(!empty($night))
     			{
     				foreach($night as $night_key => $night_val)
@@ -356,24 +374,15 @@ class Databases_Joins_Reports
     					$result_night_val[] = $night_val;
     				}
     			}
-    
-    			if(!empty($midnight))
-    			{
-    				foreach($midnight as $midnight_key => $midnight_val)
-    				{
-    					$result_midnight_key[] = $midnight_key;
-    					$result_midnight_val[] = $midnight_val;
-    				}
-    			}
     		}
-    
+    		
     		$data = $this->db->select();
     		$data->from("report-rev", array("hour(order_time) as dt", "sum(order_amount) as amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -390,7 +399,7 @@ class Databases_Joins_Reports
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -399,20 +408,43 @@ class Databases_Joins_Reports
     				$result_val[] = $row['amount'];
     			}
     		}
-    
+    		
+    		//计算人数和人均消费
+    		$data = $this->db->select();
+    		$data->from("report-rev", array("month(order_time) as dt", "sum(order_amount) as amount", "sum(customer_qty) as qty"));
+    		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
+    		$data->where("order_channel = ?", 1); //仅限堂食
+    		$data->group("dt");
+    		$data->order("dt ASC");
+    		$rows = $this->db->fetchAll($data);
+    		
+    		if(!empty($rows))
+    		{
+    			foreach($rows as $row)
+    			{
+    				if($row['qty'])
+    				{
+    					$result_customer_qty[] = $row['qty'];
+    					$result_avg_sale[] = $mod_plugin->FormatPrice($row['amount'] / $row['qty']);
+    				}else{
+    					$result_customer_qty[] = 0;
+    					$result_avg_sale[] = 0;
+    				}
+    			}
+    		}
+    		
     		//按时段
     		$data = $this->db->select();
     		$data->from("report-rev", array("order_time", "order_amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		/*
-    		 * lunch 08:00 - 13:59
-    		* afternoon 14:00 - 16:59
-    		* dinner 17:00 - 19:59
-    		* night 20:00 - 21:59
-    		* midnight 22:00 - 01:59
-    		*/
+    		 * lunch 09:00 - 14:00
+    		 * afternoon 14:00 - 17:00
+    		 * dinner 17:00 - 21:00
+    		 * night 21:00 - 01:00
+    		 */
     		$result_lunch_key = array();
     		$result_lunch_val = array();
     		$result_afternoon_key = array();
@@ -421,72 +453,60 @@ class Databases_Joins_Reports
     		$result_dinner_val = array();
     		$result_night_key = array();
     		$result_night_val = array();
-    		$result_midnight_key = array();
-    		$result_midnight_val = array();
-    
+    		
     		$lunch = array();
     		$afternoon = array();
     		$dinner = array();
     		$night = array();
-    		$midnight = array();
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
     			{
     				$hour = substr($row['order_time'],11,2);
     				$min = substr($row['order_time'],14,2);
-    
+    		
     				$month = substr($row['order_time'],5,2);
-    
-    				if(in_array($hour, array(8,9,10,11,12,13,'08','09')))
+    		
+    				if(in_array($hour, array(9,10,11,12,13,'09')))
     				{
     					if(!$lunch[$month])
     					{
     						$lunch[$month] = 0;
     					}
-    
+    		
     					$lunch[$month] += $row['order_amount'];
-    
+    		
     				}elseif(in_array($hour, array(14,15,16)))
     				{
     					if(!$afternoon[$month])
     					{
     						$afternoon[$month] = 0;
     					}
-    
+    		
     					$afternoon[$month] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(17,18,19)))
+    		
+    				}elseif(in_array($hour, array(17,18,19,20)))
     				{
     					if(!$dinner[$month])
     					{
     						$dinner[$month] = 0;
     					}
-    
+    		
     					$dinner[$month] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(20,21)))
+    		
+    				}elseif(in_array($hour, array(21,22,23,0,'00')))
     				{
     					if(!$night[$month])
     					{
     						$night[$month] = 0;
     					}
-    
+    		
     					$night[$month] += $row['order_amount'];
-    
-    				}elseif(in_array($hour, array(22,23,0,1,'00','01')))
-    				{
-    					if(!$midnight[$month])
-    					{
-    						$midnight[$month] = 0;
-    					}
-    
-    					$midnight[$month] += $row['order_amount'];
-    
+    		
     				}
     			}
-    
+    		
     			if(!empty($lunch))
     			{
     				foreach($lunch as $lunch_key => $lunch_val)
@@ -495,7 +515,7 @@ class Databases_Joins_Reports
     					$result_lunch_val[] = $lunch_val;
     				}
     			}
-    
+    		
     			if(!empty($afternoon))
     			{
     				foreach($afternoon as $afternoon_key => $afternoon_val)
@@ -504,7 +524,7 @@ class Databases_Joins_Reports
     					$result_afternoon_val[] = $afternoon_val;
     				}
     			}
-    
+    		
     			if(!empty($dinner))
     			{
     				foreach($dinner as $dinner_key => $dinner_val)
@@ -513,7 +533,7 @@ class Databases_Joins_Reports
     					$result_dinner_val[] = $dinner_val;
     				}
     			}
-    
+    		
     			if(!empty($night))
     			{
     				foreach($night as $night_key => $night_val)
@@ -522,24 +542,15 @@ class Databases_Joins_Reports
     					$result_night_val[] = $night_val;
     				}
     			}
-    
-    			if(!empty($midnight))
-    			{
-    				foreach($midnight as $midnight_key => $midnight_val)
-    				{
-    					$result_midnight_key[] = $midnight_key;
-    					$result_midnight_val[] = $midnight_val;
-    				}
-    			}
     		}
-    
+    		
     		$data = $this->db->select();
     		$data->from("report-rev", array("hour(order_time) as dt", "sum(order_amount) as amount"));
     		$data->where("order_time >= ?", date("Y-m-d", mktime(0,0,0,date("m")-12,date("d"),date("Y"))));
     		$data->group("dt");
     		$data->order("dt ASC");
     		$rows = $this->db->fetchAll($data);
-    
+    		
     		if(!empty($rows))
     		{
     			foreach($rows as $row)
@@ -549,9 +560,11 @@ class Databases_Joins_Reports
     			}
     		}
     	}
-    	 
+    	
     	$result['result_key'] = implode('","', $result_key);
     	$result['result_val'] = implode(",", $result_val);
+    	$result['result_customer_qty'] = implode(",", $result_customer_qty);
+    	$result['result_avg_sale'] = implode(",", $result_avg_sale);
     	$result['result_hour_key'] = implode('","', $result_hour_key);
     	$result['result_hour_val'] = implode(",", $result_hour_val);
     	$result['result_lunch_key'] = implode('","', $result_lunch_key);
@@ -562,9 +575,7 @@ class Databases_Joins_Reports
     	$result['result_dinner_val'] = implode(",", $result_dinner_val);
     	$result['result_night_key'] = implode('","', $result_night_key);
     	$result['result_night_val'] = implode(",", $result_night_val);
-    	$result['result_midnight_key'] = implode('","', $result_midnight_key);
-    	$result['result_midnight_val'] = implode(",", $result_midnight_val);
-    	 
+    	
     	return $result;
     }
 }
